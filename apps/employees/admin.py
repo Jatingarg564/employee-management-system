b411827717a3f96ff564df8a8d3fd08f406e7f96
jatinsoftradix <jatin.garg@softradix.in>
2026-06-29 18:58:49 +0530

    "Created initial admin models and serializers for employees api"

--- apps/employees/admin.py
+++ apps/employees/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from apps.employees.models import Employee
 
 # Register your models here.
+
+admin.site.register(Employee)
+
--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.employees'
+    'apps.employees.apps.EmployeesConfig',
 ]
 
 MIDDLEWARE = [
